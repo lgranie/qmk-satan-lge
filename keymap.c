b83868a455d74d0c5713b9a07974b400d46ac260
lgranie <laurent.granie@gmail.com>
2019-06-10 20:28:30 +0200

    Use tapdance and compose key RCTL for accent

--- keymap.c
+++ keymap.c
@@ -1,6 +1,5 @@
 #include QMK_KEYBOARD_H
 
-
 enum {
   TD_LSFT_CAPS = 0,
   TD_RSFT_CAPS = 1,
@@ -11,118 +10,48 @@ enum {
   TD_U  = 6
 };
 
-void dance_e_accent (qk_tap_dance_state_t *state, void *user_data) {
-  if (state->count == 2) {
-    register_code (KC_NLCK);
-    unregister_code (KC_NLCK);
-    register_code (KC_LALT);
-    register_code (KC_P0);
-    unregister_code (KC_P0);
-    register_code (KC_P2);
-    unregister_code (KC_P2);
-    register_code (KC_P3);
-    unregister_code (KC_P3);
-    register_code (KC_P2);
-    unregister_code (KC_P2);
-    unregister_code (KC_LALT);
-    register_code (KC_NLCK);
-    unregister_code (KC_NLCK);
+void dance_accent(qk_tap_dance_state_t *state, uint16_t key) {
+  if (state->count > 1) {
+    register_code(KC_RCTL);
+    switch (state->count) {
+      case 2 :
+        register_code(KC_GRV);
+        unregister_code(KC_GRV);
+        break;
+      case 3 :
+        register_code(KC_QUOT);
+        unregister_code(KC_QUOT);
+        break;
+    }
+    unregister_code(KC_RCTL);
   }
-  else {
-    register_code (KC_E);
-    unregister_code (KC_E);
-  }
+  register_code (key);
+  unregister_code (key);
 }
+
 void dance_a_accent (qk_tap_dance_state_t *state, void *user_data) {
-  if (state->count == 2) {
-    register_code (KC_NLCK);
-    unregister_code (KC_NLCK);
-    register_code (KC_LALT);
-    register_code (KC_P1);
-    unregister_code (KC_P1);
-    register_code (KC_P3);
-    unregister_code (KC_P3);
-    register_code (KC_P3);
-    unregister_code (KC_P3);
-    unregister_code (KC_LALT);
-    register_code (KC_NLCK);
-    unregister_code (KC_NLCK);
-  }
-  else {
-    register_code (KC_A);
-    unregister_code (KC_A);
-  }
+  dance_accent (state, KC_A);
 }
+
+void dance_e_accent (qk_tap_dance_state_t *state, void *user_data) {
+  dance_accent (state, KC_E);
+}
+
 void dance_i_accent (qk_tap_dance_state_t *state, void *user_data) {
-  if (state->count == 2) {
-    register_code (KC_NLCK);
-    unregister_code (KC_NLCK);
-    register_code (KC_LALT);
-    register_code (KC_P0);
-    unregister_code (KC_P0);
-    register_code (KC_P2);
-    unregister_code (KC_P2);
-    register_code (KC_P3);
-    unregister_code (KC_P3);
-    register_code (KC_P6);
-    unregister_code (KC_P6);
-    unregister_code (KC_LALT);
-    register_code (KC_NLCK);
-    unregister_code (KC_NLCK);
-  }
-  else {
-    register_code (KC_I);
-    unregister_code (KC_I);
-  }
+  dance_accent (state, KC_I);
 }
+
 void dance_o_accent (qk_tap_dance_state_t *state, void *user_data) {
-  if (state->count == 2) {
-    register_code (KC_NLCK);
-    unregister_code (KC_NLCK);
-    register_code (KC_LALT);
-    register_code (KC_P0);
-    unregister_code (KC_P0);
-    register_code (KC_P2);
-    unregister_code (KC_P2);
-    register_code (KC_P4);
-    unregister_code (KC_P4);
-    register_code (KC_P2);
-    unregister_code (KC_P2);
-    unregister_code (KC_LALT);
-    register_code (KC_NLCK);
-    unregister_code (KC_NLCK);
-  }
-  else {
-    register_code (KC_O);
-    unregister_code (KC_O);
-  }
+  dance_accent (state, KC_O);
 }
+
 void dance_u_accent (qk_tap_dance_state_t *state, void *user_data) {
-  if (state->count == 2) {
-    register_code (KC_NLCK);
-    unregister_code (KC_NLCK);
-    register_code (KC_LALT);
-    register_code (KC_P0);
-    unregister_code (KC_P0);
-    register_code (KC_P2);
-    unregister_code (KC_P2);
-    register_code (KC_P4);
-    unregister_code (KC_P4);
-    register_code (KC_P9);
-    unregister_code (KC_P9);
-    unregister_code (KC_LALT);
-    register_code (KC_NLCK);
-    unregister_code (KC_NLCK);
-  }
-  else {
-    register_code (KC_U);
-    unregister_code (KC_U);
-  }
+  dance_accent (state, KC_U);
 }
 
 qk_tap_dance_action_t tap_dance_actions[] = {
   [TD_LSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_LSFT, KC_CAPS),
-  [TD_RSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_CAPS)
+  [TD_RSFT_CAPS] = ACTION_TAP_DANCE_DOUBLE(KC_RSFT, KC_CAPS),
   [TD_A] = ACTION_TAP_DANCE_FN(dance_a_accent),
   [TD_E] = ACTION_TAP_DANCE_FN(dance_e_accent),
   [TD_I] = ACTION_TAP_DANCE_FN(dance_i_accent),
